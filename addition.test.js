104f6663d7931f9295187ee0dd9bd959eccd529d
Ethan Lepareur <166405645+Tatanelprr@users.noreply.github.com>
2026-01-14 14:48:08 +0100

    Fix expected result in addition test case

--- addition.test.js
+++ addition.test.js
@@ -1,5 +1,5 @@
 const calculerAddition = require('./addition');
 
 test('calculerAddition(15, 5) doit retourner 20', () => {
-  expect(calculerAddition(15, 5)).toBe(99);
+  expect(calculerAddition(15, 5)).toBe(20);
 });
